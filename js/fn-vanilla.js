4eb5a11bf04193adf741e0736bd25cf864f384a8
jurin2 <89722981+jurin2@users.noreply.github.com>
2022-02-11 18:09:04 +0900

    Add files via upload

--- js/fn-vanilla.js
+++ js/fn-vanilla.js
@@ -26,28 +26,3 @@ function mobileForm(bodyOver,tarDis,tarOver,target){
     target.style.overflowY=tarOver;
 }
 
-// mobile login nav event
-function moNavLogin(){
-     // 로그인폼 열기
-     btnOpLog.addEventListener('click',function(){
-        let loginCfm = confirm('로그인 후 사용가능합니다.\n로그인 하시겠습니까?');
-        if(loginCfm){
-            mobileForm('hidden','block','scroll',loginEl);
-        };
-    });
-
-    // 로그인폼 닫기
-    btnClLog.addEventListener('click',function(){
-        mobileForm('scroll','none','hidden',loginEl);
-    });
-
-    // 네비게이션 열기
-    btnOpNav.addEventListener('click',function(){
-        mobileForm('hidden','block','scroll',navEl);
-    });
-
-    // 네비게이션 닫기
-    btnClNav.addEventListener('click',function(){
-        mobileForm('scroll','none','hidden',navEl);
-    });
-}
